a634e62ff6b39b3d80d1425c956263dca939c757
luckstar77 <luckstar77y@gmail.com>
2018-08-02 11:05:35 -0400

    fix: error call

--- app/middleware/isLogin.js
+++ app/middleware/isLogin.js
@@ -1,6 +1,4 @@
 module.exports = async (ctx, next) => {
-    ctx.logger.info('request data: %j', ctx.request.body);
     await next();
-    ctx.logger.info('response data: %j', ctx.body);
   };
   
--- app/router.js
+++ app/router.js
@@ -5,5 +5,6 @@
  */
 module.exports = app => {
   const { router, controller, middlewares } = app;
-  router.post('/thirdparty/login', middlewares.logRequest, middlewares.validateThirdpartyLoginBody, controller.accounts.thirdpartyLogin);
+  router.post('/thirdparty/login', middlewares.processRequest, middlewares.validateThirdpartyLoginBody, controller.accounts.thirdpartyLogin);
+  router.post('/user', middlewares.processRequest, middlewares.isLogin, controller.users.getUser);
 };
