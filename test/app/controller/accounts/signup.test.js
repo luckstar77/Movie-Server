40eed26c47062bd8973a60f51f70e0dfa9675e99
luckstar77 <luckstar77y@gmail.com>
2018-08-31 15:09:15 -0400

    fix: test

--- test/app/controller/accounts/signup.test.js
+++ test/app/controller/accounts/signup.test.js
@@ -2,7 +2,7 @@
 
 const { app } = require('egg-mock/bootstrap');
 
-describe.only('test/app/controller/accounts/signup.test.js', () => {
+describe('test/app/controller/accounts/signup.test.js', () => {
   before(async () => {
     const ctx = app.mockContext();
     await ctx.service.users.deleteAll();
